1fd345ea671ef6aa48f69e7dedd76c30337f3409
Jonathan Palalay <jonopalalay@gmail.com>
2022-01-27 13:43:01 -0600

    fullName luckyNumber completed

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,16 +1,22 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
+let firstName = 'Jonathan ';
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Palalay'
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
+let fullName = firstName  + lastName;
 
 // 4 - Console log the value of `fullName`
-
+console.log (fullName);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
+let luckyNumber = '11 '
+// 6 - Console log this sentence, adding in the variables you created above:
 
-// 6 - Console log this sentence, adding in the variables you created above: 
+let sentence = 'My name is ' + fullName + ', and I think ' + luckyNumber + 'is a winner!'
+console.log ( sentence );
+
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
 
@@ -26,14 +32,14 @@
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 
-// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
+// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
 
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
 
-// 15 - Write a conditional that console logs "I can have more pets!" 
+// 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
@@ -44,7 +50,7 @@
 // 16 - Make a variable called `mostPets` and a conditional that
 // correctly checks the `pets` and `friendsPets` variables, and
 // assigns the highest value to `mostPets`. There's several possibilities --
-// be sure to think through all the scenarios. 
+// be sure to think through all the scenarios.
 // console.log `mostPets` after the conditional has run.
 
 // 17 - Write a *switch* statement that logs:
@@ -55,5 +61,3 @@
 //      You'll need to research how to use switch statements!
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-
-
